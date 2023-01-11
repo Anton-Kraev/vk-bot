382e3f3cfde702d7c94a865df9dd06e47afec0e3
Anton Kraev <antonkraeww@gmail.com>
2023-01-11 20:10:48 +0300

    added exam and script for renaming

--- watermark.py
+++ watermark.py
@@ -30,32 +30,34 @@ logo.putdata(newData)
 logoWidth = logo.width
 logoHeight = logo.height
 
+for dirname, _, filenames in os.walk(path):
+    for filename in filenames:
+        file = dirname.replace('\\', '/') + '/' + filename
+        if filename[-6:-4] != '-0':
+            image = Image.open(file)
+            imageWidth = image.width
+            imageHeight = image.height
 
-for filename in os.listdir(path):
-    if any([filename.lower().endswith(ext) for ext in EXTS]) and filename != lgo:
-        image = Image.open(path + '/' + filename)
-        imageWidth = image.width
-        imageHeight = image.height
+            try:
+                if pos == 'topleft':
+                    image.paste(logo, (0, 0), logo)
+                elif pos == 'topright':
+                    image.paste(logo, (imageWidth - logoWidth, 0), logo)
+                elif pos == 'bottomleft':
+                    image.paste(logo, (0, imageHeight - logoHeight), logo)
+                elif pos == 'bottomright':
+                    image.paste(logo, (imageWidth - logoWidth, imageHeight - logoHeight), logo)
+                elif pos == 'center':
+                    image.paste(logo, ((imageWidth - logoWidth) / 2, (imageHeight - logoHeight) / 2), logo)
+                else:
+                    print('Error: ' + pos + ' is not a valid position')
+                    print(
+                        'Usage: watermark.py \'image path\' \'logo path\' [topleft, topright, bottomleft, bottomright, center]')
 
-        try:
-            if pos == 'topleft':
-                image.paste(logo, (0, 0), logo)
-            elif pos == 'topright':
-                image.paste(logo, (imageWidth - logoWidth, 0), logo)
-            elif pos == 'bottomleft':
-                image.paste(logo, (0, imageHeight - logoHeight), logo)
-            elif pos == 'bottomright':
-                image.paste(logo, (imageWidth - logoWidth, imageHeight - logoHeight), logo)
-            elif pos == 'center':
-                image.paste(logo, ((imageWidth - logoWidth)/2, (imageHeight - logoHeight)/2), logo)
-            else:
-                print('Error: ' + pos + ' is not a valid position')
-                print('Usage: watermark.py \'image path\' \'logo path\' [topleft, topright, bottomleft, bottomright, center]')
+                image.save(file)
+                print('Added watermark to ' + file)
 
-            image.save(path + '/' + filename)
-            print('Added watermark to ' + path + '/' + filename)
-
-        except:
-            image.paste(logo, (int((imageWidth - logoWidth)/2), int((imageHeight - logoHeight)/2)), logo)
-            image.save(path + '/' + filename)
-            print('Added default watermark to ' + path + '/' + filename)
+            except:
+                image.paste(logo, (int((imageWidth - logoWidth) / 2), int((imageHeight - logoHeight) / 2)), logo)
+                image.save(file)
+                print('Added default watermark to ' + file)
